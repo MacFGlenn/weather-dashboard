c75d215ad2f735cf6497ff41feb322979a7ad8cb
MFG <macfglenn@gmail.com>
2021-11-06 05:59:24 -0400

    all the info for the 5-day forecast now appears after a search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var currentDayEl = document.querySelector("#current-day");
+var fiveDayEl = document.querySelector("#five-day");
 
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -27,37 +28,91 @@ function getWeatherInfo(city) {
     if (response.ok) {
       response.json().then(function (data) {
         displayWeather(data, city);
+        getFiveDay(city);
+      });
+    }
+  });
+}
+
+function getFiveDay(city) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=440f6e98bfd6d64aa6e6b8974736171d";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayFiveDoor(data);
       });
     }
   });
 }
 
 function displayWeather(weather, city) {
-    currentDayEl.innerHTML = "";
+  currentDayEl.innerHTML = "";
 
-    // display searched city name, temp, wind, humidity, and uv
-    var currentDayName = document.createElement("h2");
-    currentDayName.textContent = city;
+  // display searched city name, temp, wind, humidity, and uv
+  var currentDayName = document.createElement("h2");
+  currentDayName.textContent = city;
 
-    var currentDayTemp = document.createElement("h4");
-    currentDayTemp.textContent = "Temp: " + weather.main.temp;
+  var currentDayTemp = document.createElement("h4");
+  currentDayTemp.textContent = "Temp: " + weather.main.temp;
 
-    var currentDayWind = document.createElement("h4");
-    currentDayWind.textContent = "Wind: " + weather.wind.speed;
+  var currentDayWind = document.createElement("h4");
+  currentDayWind.textContent = "Wind: " + weather.wind.speed;
 
-    var currentDayHum = document.createElement("h4");
-    currentDayHum.textContent = "Humidity: " + weather.main.humidity;
+  var currentDayHum = document.createElement("h4");
+  currentDayHum.textContent = "Humidity: " + weather.main.humidity;
 
-    // is there supposed to be a uv index cause I cant find it
-    // var currentDayUV = document.createElement("h4");
-    // currentDayUV = weather.;
+  // is there supposed to be a uv index cause I cant find it
+  // var currentDayUV = document.createElement("h4");
+  // currentDayUV = weather.;
 
-    currentDayEl.appendChild(currentDayName);
-    currentDayEl.appendChild(currentDayTemp);
-    currentDayEl.appendChild(currentDayWind);
-    currentDayEl.appendChild(currentDayHum);
-    // currentDayEl.appendChild(currentDayUV);
-//   }
+  currentDayEl.appendChild(currentDayName);
+  currentDayEl.appendChild(currentDayTemp);
+  currentDayEl.appendChild(currentDayWind);
+  currentDayEl.appendChild(currentDayHum);
+  // currentDayEl.appendChild(currentDayUV);
+  //   }
+}
+
+function displayFiveDoor(weather) {
+  fiveDayEl.innerHTML = "";
+
+  var header = document.createElement("h3");
+  header.textContent = "5-Day Forecast";
+  fiveDayEl.appendChild(header);
+
+  var dayOneDiv = document.createElement("div");
+  fiveDayEl.appendChild(dayOneDiv);
+
+  for (i = 0; i <= weather.list.length; i++) {
+    // date
+    var date = document.createElement("h4");
+    date.textContent = JSON.stringify(weather.list[i*8].dt_txt);
+
+    // icon
+
+    // temp
+    var temp = document.createElement("h4");
+    temp.textContent =
+      "Temp: " + JSON.stringify(weather.list[i*8].main.temp);
+
+    // wind speed
+    var windSpeed = document.createElement("h4");
+    windSpeed.textContent = "Wind Speed: " + JSON.stringify(weather.list[i*8].wind.speed);
+
+    // humidity
+    var hum = document.createElement("h4");
+    hum.textContent =
+      "Humidity: " + JSON.stringify(weather.list[i*8].main.humidity);
+
+    dayOneDiv.appendChild(date);
+    dayOneDiv.appendChild(temp);
+    dayOneDiv.appendChild(windSpeed);
+    dayOneDiv.appendChild(hum);
+  }
 }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
